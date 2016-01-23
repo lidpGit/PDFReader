b4a440fa296ab429592237b2e18e110a51c81b11
lidp <lidaipeng@lidpdemacbook-pro.local>
2016-01-24 05:16:48 +0800

    Initial Commit

---
+++ PDFReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDFReader
+//
+//  Created by lidp on 16/1/24.
+//  Copyright © 2016年 lidp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDFReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDFReader
+//
+//  Created by lidp on 16/1/24.
+//  Copyright © 2016年 lidp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
